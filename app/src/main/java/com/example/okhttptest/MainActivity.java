22c062ef47c2c419de4cb9b791fe91a663f3a48c
Crowd <crowd7@gmail.com>
2017-07-29 10:29:29 +0800

    2nd commit

--- app/src/main/java/com/example/okhttptest/MainActivity.java
+++ app/src/main/java/com/example/okhttptest/MainActivity.java
@@ -2,12 +2,63 @@ package com.example.okhttptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnGet;
+    private TextView tvResult;
+
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findUId();
+
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                executor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        final OkHttpClient client = new OkHttpClient();
+                        Request request = new Request.Builder()
+                                .url("https://true-music-142601.appspot.com/content?type=westmovie")
+                                .build();
+
+                        try {
+                            final Response response = client.newCall(request).execute();
+                            final String result = response.body().string();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    tvResult.setText(result);
+                                }
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void findUId() {
+        btnGet = (Button) findViewById(R.id.btnGet);
+        tvResult = (TextView) findViewById(R.id.tvResult);
     }
 }
